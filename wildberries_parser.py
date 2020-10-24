1004d1c750e4c3aab640dca87b74e3320e9784d0
Acid Wizard <adwiz@yandex.ru>
2020-10-24 11:10:51 +0300

    first path completed

--- wildberries_parser.py
+++ wildberries_parser.py
@@ -6,6 +6,7 @@ import lxml
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('wilberries')
 
+
 class Client:
 
     def __init__(self):
@@ -16,9 +17,27 @@ class Client:
         }
 
     def load_page(self, page: int=None):
-        url = 'https://www.wildberries.ru/search?text=%D1%88%D0%B5%D0%BB%D0%BA%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%BF%D0%BB%D0%B0%D1%82%D0%BE%D0%BA'
+        url = 'https://www.wildberries.ru/search?text=%D1%88%D0%B5%D0%BB%D0%BA%D0%BE%D0%B2%D1%8B%' \
+              'D0%B9%20%D0%BF%D0%BB%D0%B0%D1%82%D0%BE%D0%BA'
         result = self.session.get(url=url)
         result.raise_for_status()
         return result.text
 
     def parse_page(self, text: str):
+        soup = bs4.BeautifulSoup(text, 'lxml')
+        container = soup.select('div.dtList.i-dtList.j-card-item')
+        for block in container:
+            self.parse_block(block=block)
+
+    def parse_block(self, block):
+        logger.info(block)
+        logger.info('=' * 100)
+
+    def run(self):
+        text = self.load_page()
+        self.parse_page(text=text)
+
+
+if __name__ == '__main__':
+    parser = Client()
+    parser.run()
