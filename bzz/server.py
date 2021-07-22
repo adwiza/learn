03d80b304ad09085c6748784540faabe73994012
Adwiz <adwiz@suroviy.ru>
2021-07-22 07:33:52 +0300

    learn bottle

--- bzz/server.py
+++ bzz/server.py
@@ -7,15 +7,6 @@ def my_func():
     return "OK"
 
 
-@route("/jdva")
-def my_func():
-    return {
-        "my_saying": {
-            "test": "ываываываываываываы",
-            "test2": choice(
-                ["awdeaqw", 2, "awdaw", 80]
-            ),
-        }
-    }
+
 
 run()
